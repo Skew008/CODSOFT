71dc6a18d6e8ae622ef05539bd91010d45a1575b
Skew008 <shreyanshtelrandhe23@gmal.com>
2024-03-07 21:00:40 +0530

    Game Completed

---
+++ first-task/src/main/java/com/example/Game.java
@@ -0,0 +1,67 @@
+package com.example;
+
+import java.util.*;
+
+public class Game {
+  List<Integer> a;
+  Random rd;
+
+  public Game()
+  {
+    a = new ArrayList<>();
+    rd = new Random();
+  }
+
+  public void playgame()
+  {
+    System.out.println("Game Start:");
+    int round = 1;
+    Scanner sc = new Scanner(System.in);
+    while(true)
+    {
+      int r = rd.nextInt()%100;
+      int score = 10;
+      System.out.println("\nRound "+round+":");
+      for(int i=10; i>0; i--)
+      {
+        System.out.print("\nEnter your guess:");
+        int guess = sc.nextInt();
+        if(guess==r)
+        {
+          System.out.println("You guessed it correctly!!!");
+          break;
+        }
+        else if(guess<r)
+          System.out.println("Guess is too low.");
+        else if(guess>r)
+        System.out.println("Guess is too high.");
+        score--;
+      }
+      if(score==0)
+        System.out.println("\nDid not win in this round :(");
+      a.add(score);
+      round++;
+      System.out.print("Want to play Round "+round+"? ");
+      String s = sc.next();
+      if(s.equalsIgnoreCase("no"))
+        break;
+    }
+    printresult(a);
+    System.out.println("Happy Playing :)\n See you again soon!!!");
+    sc.close();
+  }
+
+  private void printresult(List<Integer> a)
+  {
+    System.out.println("\n\nScores of the game:");
+    int round = 1;
+    for(int i:a)
+    {
+      if(i==0)
+        System.out.println("Round "+round+": Score:0, Lost");
+      else
+        System.out.println("Round "+round+" Score:"+i+", Won");
+      round++;
+    }
+  }
+}
